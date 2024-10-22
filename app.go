2957faac251bd665c4a24b8716107a46df183a9a
Eiichi Sayama <eiichimouse@outlook.jp>
2024-10-23 00:12:16 +0900

    feat:Read data from yaml file

--- app.go
+++ app.go
@@ -3,11 +3,28 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+}
+
+type Questions struct {
+	Stages Stages `yaml:"phase"`
+}
+
+type Stages struct {
+	Early  []string `yaml:"early"`
+	Middle []string `yaml:"middle"`
+	Late   []string `yaml:"late"`
+}
+
+type Sample struct {
+	SampleText string `yaml:"sample"`
 }
 
 // NewApp creates a new App application struct
@@ -24,4 +41,18 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) StartInterview(company string) string {
+	var questionSets Questions
+	yamlFile, err := os.ReadFile("./data/company.yaml")
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(yamlFile, &questionSets)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("questionSets is :", questionSets)
+	return fmt.Sprintf("yml file is : %s", questionSets)
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import {useState} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import {Greet,StartInterview} from "../wailsjs/go/main/App";
 
 function App() {
     const [resultText, setResultText] = useState("Please enter your name below 👇");
+    const [questions, setQuestions] = useState("")
     const [name, setName] = useState('');
     const updateName = (e: any) => setName(e.target.value);
     const updateResultText = (result: string) => setResultText(result);
+    const updateQuestions = (yml:string)=>setQuestions(yml)
 
     function greet() {
         Greet(name).then(updateResultText);
+    }
+
+    function startinterview(){
+        StartInterview("").then()
     }
 
     return (
@@ -20,6 +26,12 @@ function App() {
             <div id="input" className="input-box">
                 <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
                 <button className="btn" onClick={greet}>Greet</button>
+            </div>
+            <div>
+                Questions from yml file : {questions}
+            </div>
+            <div>
+                <button className="btn" onClick={startinterview}>Read yaml</button>
             </div>
         </div>
     )

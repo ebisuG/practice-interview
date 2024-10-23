966ce25719115c18d1405bf2a9d522b7f7b0c68f
Eiichi Sayama <eiichimouse@outlook.jp>
2024-10-23 11:40:51 +0900

    feat:Display data from yaml file on Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,22 @@ import {useState} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
 import {Greet,StartInterview} from "../wailsjs/go/main/App";
+import {main} from "../wailsjs/go/models"
 
 function App() {
     const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [questions, setQuestions] = useState("")
+    const [questions, setQuestions] = useState<main.Questions>()
     const [name, setName] = useState('');
     const updateName = (e: any) => setName(e.target.value);
     const updateResultText = (result: string) => setResultText(result);
-    const updateQuestions = (yml:string)=>setQuestions(yml)
+    const updateQuestions = (yml:main.Questions)=>setQuestions(yml)
 
     function greet() {
         Greet(name).then(updateResultText);
     }
 
     function startinterview(){
-        StartInterview("").then()
+        StartInterview("").then(updateQuestions)
     }
 
     return (
@@ -28,7 +29,9 @@ function App() {
                 <button className="btn" onClick={greet}>Greet</button>
             </div>
             <div>
-                Questions from yml file : {questions}
+                Early Questions from yml file : {questions?.Stages.Early}<br></br>
+                Middle Questions from yml file : {questions?.Stages.Middle}<br></br>
+                Late Questions from yml file : {questions?.Stages.Late}
             </div>
             <div>
                 <button className="btn" onClick={startinterview}>Read yaml</button>

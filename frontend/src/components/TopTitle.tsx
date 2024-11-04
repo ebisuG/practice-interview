668ffbf38e2b7343a085be1f8c9c90016077b780
Eiichi Sayama <eiichimouse@outlook.jp>
2024-11-04 14:42:02 +0900

    style:Improve style and put more sentences

--- frontend/src/components/TopTitle.tsx
+++ frontend/src/components/TopTitle.tsx
@@ -1,8 +1,9 @@
 function TopTitle() {
     return (<>
 
-        <div className="m-14 text-center">
-            Choose company.
+        <div className="mt-14 text-center">
+            Choose company and edit questions.<br />
+            After editing, press start and let's practice interview.
         </div>
     </>)
 

b598cb4f20faa0abe8c1e117d2afa20e4fa44246
Eiichi Sayama <eiichimouse@outlook.jp>
2024-11-06 00:01:09 +0900

    feat:Add a button to skip the question user is seeing

--- frontend/src/components/DisplayQuestion.tsx
+++ frontend/src/components/DisplayQuestion.tsx
@@ -46,10 +46,21 @@ function DisplayQuestion(props: props) {
         return result
     }
 
+    function goNextQuestion() {
+        setInterviewQuestionIndex(interviewQuestionIndex + 1)
+    }
+
     return (
         <div id="DisplayQuestion">
-            <div className='min-h-7'>
-                {interviewQuestion[interviewQuestionIndex]}
+            <div className='flex items-center flex-col gap-2'>
+                <div className='min-h-7 max-w-[600px] border-2 rounded border-amber-50 bg-amber-50 p-2'>
+                    {interviewQuestion[interviewQuestionIndex]}
+                </div>
+                <div className='w-14 text-center cursor-pointer
+                bg-amber-200 hover:bg-amber-400 
+                    rounded border-amber-200 hover:border-amber-400 border-2 p-2' onClick={goNextQuestion}>
+                    Next
+                </div>
             </div>
         </div>
     )

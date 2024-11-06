152d8638ee68e4b9d21e7b26265377484ff6eeff
Eiichi Sayama <eiichimouse@outlook.jp>
2024-11-06 16:55:51 +0900

    feat:Add a link to top page

--- frontend/src/components/DisplayQuestion.tsx
+++ frontend/src/components/DisplayQuestion.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { main } from "../../wailsjs/go/models"
 import { ReadQuestionFile } from '../../wailsjs/go/main/App';
+import { Link } from 'react-router-dom';
 
 interface props {
     filePath: string | null
@@ -56,10 +57,19 @@ function DisplayQuestion(props: props) {
                 <div className='min-h-7 max-w-[600px] border-2 rounded border-amber-50 bg-amber-50 p-2'>
                     {interviewQuestion[interviewQuestionIndex]}
                 </div>
-                <div className='w-14 text-center cursor-pointer
+                <div className='flex justify-center gap-2'>
+                    <div className='w-14 text-center cursor-pointer
                 bg-amber-200 hover:bg-amber-400 
-                    rounded border-amber-200 hover:border-amber-400 border-2 p-2' onClick={goNextQuestion}>
-                    Next
+                rounded border-amber-200 hover:border-amber-400 border-2 p-2' onClick={goNextQuestion}>
+                        Next
+                    </div>
+                    <Link to={"/"}>
+                        <div className='w-14 text-center cursor-pointer
+                bg-lime-200 hover:bg-lime-400 
+                rounded border-lime-200 hover:border-lime-400 border-2 p-2'>
+                            Top
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
